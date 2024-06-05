175433a7fe55e2bc992c8cb5443c0cef21fe1193
darelljay <darelljason1@gmail.com>
2024-06-05 14:10:10 +0900

    feat: set middleware function

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
 export class Middleware {
-  public auth = (req: Request, res: Response, next: NextFunction) => {
-    if (req.session && req.session.isLoggedIn) {
-      next();
-    } else {
-      res.status(401).json({ message: 'Unauthorized' });
+    public auth = (req: any, res: Response, next: NextFunction) => {
+        if (req.session && req.session.isLoggedIn) {
+            next();
+        } else {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
     }
-  };
 }
