85c4fb7b0ac36959ef5f69a1661ea8c6c9291c45
darelljay <darelljason1@gmail.com>
2024-06-07 22:06:35 +0900

    fixing error

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -1,11 +1,12 @@
-import session from 'express-session';
+import 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
-    isLoggedIn: boolean;
+    user: { userId: string; uuId: string };
+    isLoggin: boolean;
   }
 }
-
+  
 declare module 'express-serve-static-core' {
   interface Request {
     session: session.Session & Partial<session.SessionData>;
