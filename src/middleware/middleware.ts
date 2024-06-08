e3b08494062c43c3a3905c491b71d16598b7b6eb
darelljay <darelljason1@gmail.com>
2024-06-08 10:17:28 +0900

    fix:  Session error fixed

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
 export class Middleware {
-    public auth = (req: any, res: Response, next: NextFunction) => {
-        if (req.session && req.session.isLoggedIn) {
+    public auth = (req: Request, res: Response, next: NextFunction) => {
+        if (req.session && (req.session as any).user?.isLoggin) {
             next();
         } else {
             res.status(401).json({ message: 'Unauthorized' });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,54 @@
 import { userService } from "./user.Service";
-import { Request,Response } from "express";
+import { NextFunction, Request,Response } from "express";
+import {Middleware} from '../middleware/middleware'
+import { User } from "../interface/user";
 export class userController extends userService{
+    private middleWare:Middleware;
     constructor(){
         super();
+        this.middleWare = new Middleware();
     }
 
     public createNewUser = async(req:Request,res:Response):Promise<void>=>{
         const {userName,password} = req.body;
         try {
-            const result = await this.createNewUser(userName,password);
-            res.status(200).json(result);
+            const result = await this.createUser(userName,password);
+            if(typeof(result) === 'string'){
+                res.status(400).json({"msg":result})
+            }else{
+                res.status(201).json({msg:"User Created"});
+            }
         } catch (error) {
             console.error(error);
             res.status(500).json({"msg":"Error while Creating User"});
         }
     }
 
+    public findExistingUser = async(req:Request,res:Response):Promise<void>=>{
+        const {userName} = req.query;
+        console.log(req.query)
+        try{
+            const result = await this.findUser(userName as string);
+            result ? res.status(200).json(result):res.status(404).json({"msg":"could not find user"});
+        }catch(error){
+            console.error(error);
+            res.status(500).json({"msg":"Error while finding User"});
+        }
+    }
+
+    public login = async (req: Request, res: Response): Promise<void> => {
+        try {
+          const { userName, userPassword } = req.body;
+          const loginUser = await this.loginUser(userName, userPassword) as unknown as User;
+          if (loginUser) {
+            (req.session as any).user  = { userId: loginUser.user_id, uuId: loginUser.uuId,isLoggin:true };
+            res.status(200).json({ "msg": "Login successful" });
+          } else {
+            res.status(401).json({ "msg": "Invalid credentials" });
+          }
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ "msg": "Error while finding User" });
+        }
+      }
 }
