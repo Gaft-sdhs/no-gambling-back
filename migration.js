e39b51a9ea0f0f81110755ec4c997587dc597a29
darelljay <darelljason1@gmail.com>
2024-06-17 18:02:25 +0900

    fix: migration code

--- migration.js
+++ migration.js
@@ -2,7 +2,6 @@ const { v4: uuIdv4 } = require("uuid");
 const { faker } = require("@faker-js/faker");
 const mysql2 = require("mysql2/promise");
 const dotenv = require("dotenv");
-const { stat } = require("fs");
 dotenv.config();
 
   const create_user_table = `
@@ -63,16 +62,15 @@ dotenv.config();
 
 
 const generatePost = (num) =>{
-
+    const title = faker.lorem.word({min:10,max:45});
     const owner = faker.internet.userName();
     const content = faker.lorem.paragraph();
     const views = faker.number.int({min: 0,max:100000 });
     const upload_date = faker.date.past().toISOString().split('T')[0];
     const uuId = uuIdv4();
-    const statement =`INSERT INTO posts_table (owner, content, views, upload_date, uuId) VALUES ('${owner}', '${content}', ${views}, '${upload_date}', '${uuId}');`;
+    const statement =`INSERT INTO posts_table (title,owner, content, views, upload_date, uuId) VALUES ('${title}','${owner}', '${content}', ${views}, '${upload_date}', '${uuId}');`;
     // executeQuery(statement);
-  
-    return {sql:statement,data: {owner,content,views,upload_date,uuId}};
+    return {sql:statement,data: {title,owner,content,views,upload_date,uuId}};
 }
 
 // generateUserInertMents(100);
@@ -103,7 +101,7 @@ const insrtRandomUserAndPost = async(loopNum)=>{
     } catch (error) {
      console.error(error); 
     }finally{
-      connection.end()
+      // connection.end()
     }
   }
 }
